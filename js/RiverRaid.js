3a6d08bccc64d1365454f9039b1efcd732ad960c
gasperpb <achillespb@hotmail.com>
2021-01-27 14:25:29 -0300

    Fisrt commit

---
+++ js/RiverRaid.js
@@ -0,0 +1,6 @@
+//Recuperando referência dos objetos no documento
+var canvas = document.getElementById("tela");
+var ctx = canvas.getContext("2d");
+//Um pequeno teste (remover depois de testar)
+ctx.fillStyle = "#FF0000"; //Usar cor vermelha
+ctx.fillRect(20, 30, 50, 100); //x=20, y=30, w=50 e h=100
